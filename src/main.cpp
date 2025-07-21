d0021646d73e5b1086ee22ed423d6fbc5cbd9c51
Ryuthecoder <ryu_tat@icloud.com>
2025-07-21 16:09:47 +0900

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,13 @@ pros::adi::Pneumatics littlewill('b', false); //little will mech */
 pros::Imu imu(7);
 
 // tracking wheels
-// horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed
+
 pros::Rotation horizontalEnc(20);
-// vertical tracking wheel encoder. Rotation sensor, port 11, reversed
+
 pros::Rotation verticalEnc(-11);
-// horizontal tracking wheel. 2" diameter, 5.75" offset, back of the robot (negative)
+
 lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_2, 0);
-// vertical tracking wheel. 2" diameter, 2.5" offset, left of the robot (negative)
+
 lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, 0);
 
 // drivetrain settings
@@ -90,24 +90,21 @@ lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
 lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors, &throttleCurve, &steerCurve);
 
 /**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
+Runs initialization code
+this occurs as soon as the program is started
+all other competition modes are blocked by initialize
+it is recommended to keep execution time for this mode under a few seconds*/
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
 
-    // the default rate is 50. however, if you need to change the rate, you
-    // can do the following.
+    // the default rate is 50. however, it can be changed using the following line
     // lemlib::bufferedStdout().setRate(...);
-    // If you use bluetooth or a wired connection, you will want to have a rate of 10ms
+    // If I use bluetooth or a wired connection, I will want to have a rate of 10ms
 
-    // for more information on how the formatting for the loggers
-    // works, refer to the fmtlib docs
+ 
 
-    // thread to for brain screen and position logging
+    //brain screen and position logging
     pros::Task screenTask([&]() {
         while (true) {
             // print robot location to the brain screen
@@ -122,70 +119,41 @@ void initialize() {
     });
 }
 
-/**
- * Runs while the robot is disabled
- */
+
+//Runs while the robot is disabled
+
 void disabled() {}
 
-/**
- * runs after initialize if the robot is connected to field control
- */
+
+//runs after initialize if the robot is connected to field control
+
 void competition_initialize() {}
 
 // get a path used for pure pursuit
-ASSET(PLACEHOLDER_pathjerryio_txt); // '.' replaced with "_" to make c++ happy
+ASSET(PLACEHOLDER_pathjerryio_txt); // replace "." with "_" to comply with C++ 
 
-/**
- * Runs during auto
- */
+//runs during auton
+
 void autonomous() {
-    
+    //pure pursuit path
 	chassis.follow(PLACEHOLDER_pathjerryio_txt, 20, 11000);
-	/*// Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
-    // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // cancel the movement after it has traveled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
-    // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // Turn to face a direction of 90º. Timeout set to 1000
-    // will always be faster than 100 (out of a maximum of 127)
-    // also force it to turn clockwise, the long way around
-    chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
-	*/
 
 }
 
-/**
- * Runs in driver control
- */
+
+// driver control
 void opcontrol() {
-    // controller
+    // controller 
     
-    // loop to continuously update motors
+    // loop to continuously update motors im still exploring options
 
     
 
     while (true) {
-        // get joystick positions
+        
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-        // move the chassis with curvature drive
+        
         chassis.arcade(leftY, rightX);
         
         /**if (leftY == 0 && rightX == 0) {
